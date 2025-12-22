4a297cf981f5b0508f189081b605da2fc28422bf
mehrez-choura <mehrezchoura@gmail.com>
2025-12-22 10:40:50 +0100

    Improve comment on base path in vite.config.js
    Updated base path comment for clarity.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './', // <--- L'astuce est ici : un point et un slash. Cela force le chemin relatif.
 })
